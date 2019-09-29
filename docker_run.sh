b99ad005643152793d99b7718b6d2795c30ab38f
Mitchel Humpherys <mitch@directangular.com>
2019-09-29 12:06:07 -0700

    docker_run.sh: Add env var to specify storage directory
    Currently `docker_run.sh` bind-mounts some directories rooted at the
    working directory when the script is invoked.  Add an environment variable
    so that this storage location can be specified at invocation time without
    having to change to a different directory.

--- docker_run.sh
+++ docker_run.sh
@@ -2,14 +2,16 @@
 
 # https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
 
+PIHOLE_BASE=${PIHOLE_BASE:-$(pwd)}
+
 docker run -d \
     --name pihole \
     -p 53:53/tcp -p 53:53/udp \
     -p 80:80 \
     -p 443:443 \
     -e TZ="America/Chicago" \
-    -v "$(pwd)/etc-pihole/:/etc/pihole/" \
-    -v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+    -v "${PIHOLE_BASE}/etc-pihole/:/etc/pihole/" \
+    -v "${PIHOLE_BASE}/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
     --dns=127.0.0.1 --dns=1.1.1.1 \
     --restart=unless-stopped \
     pihole/pihole:latest
